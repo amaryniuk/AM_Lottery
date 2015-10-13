54b116638adebe4f593250a23ee9e76b1a4a087e
Alan <amaryniuk@gmail.com>
2015-10-12 20:56:52 -0400

    oct 12. done

--- src/Lottery.java
+++ src/Lottery.java
@@ -4,10 +4,11 @@ import java.util.*;
 public class Lottery {
 
     // Add tickets to an array, then shuffle the array, then sell them in that order
-    static int numTickets = 10;
+    static int numTickets = 50;
 
     // for new game
     static int ticketsSold = 0;
+    static Boolean drawDone = false;
     static List<Integer> ticketList = new ArrayList<>();
     static List<Ticket> ticketsSoldList = new ArrayList<>();
 
@@ -28,33 +29,37 @@ public class Lottery {
         Scanner scanner = new Scanner(System.in);
         while ((command = scanner.nextLine()) != null) {
 
-            if (ticketsSold >= numTickets) {
-                System.out.println("Tickets are all sold out. Time for a draw!");
-                command = "draw";
-            }
-
             if ("Purchase".equalsIgnoreCase(command) || "p".equalsIgnoreCase(command)) {
-                System.out.println("What is the buyer's name?");
-                buyerName = scanner.nextLine();
-                ballNumberChosen = ticketList.get(ticketsSold);
-                ticketsSoldList.add(new Ticket(buyerName, ballNumberChosen));
-                ticketsSold++;
-                pot = pot.add(ticketCost);
-                System.out.println(buyerName + ", your ticket number is: " + ballNumberChosen + ". Good Luck!");
+                if (ticketsSold >= numTickets) {
+                    System.out.println("Tickets are sold out, time to do a draw.");
+                } else if (drawDone) {
+                    System.out.println("Draw already done. Show winners or start a new game.");
+                } else {
+                    System.out.println("What is the buyer's name?");
+                    buyerName = scanner.nextLine();
+                    ballNumberChosen = ticketList.get(ticketsSold);
+                    ticketsSoldList.add(new Ticket(buyerName, ballNumberChosen));
+                    ticketsSold++;
+                    pot = pot.add(ticketCost);
+                    System.out.println(buyerName + ", your ticket number is: " + ballNumberChosen + ". Good Luck!");
+                }
             }
 
             else if ("Draw".equalsIgnoreCase(command) || "d".equalsIgnoreCase(command)) {
-                System.out.println("\nDraw is happening! The total pot for this round is: $" + pot.toPlainString());
-                Draw d = new Draw(numTickets);
-                d.showBalls();
+                if (drawDone) {
+                    System.out.println("Draw already done. Show winners or start a new game.");
+                } else {
+                    System.out.println("\nDraw is happening! The total pot for this round is: $" + pot.toPlainString());
+                    Draw d = new Draw(numTickets);
+                    drawDone = true;
+                    d.showBalls();
 
-                firstBall = d.get1Ball();
-                secondBall = d.get2Ball();
-                thirdBall = d.get3Ball();
+                    firstBall = d.get1Ball();
+                    secondBall = d.get2Ball();
+                    thirdBall = d.get3Ball();
 
-                findWinners(pot);
-                pot = payoutWinners(pot);
-                newGame();
+                    findWinners(pot);
+                }
             }
 
             else if ("Winners".equalsIgnoreCase(command) || "w".equalsIgnoreCase(command)) {
@@ -88,36 +93,29 @@ public class Lottery {
             tics.add(i);
         }
         Collections.shuffle(tics);
-        System.out.println(tics);
+//        System.out.println(tics);
     }
 
     public static void newGame() {
-        System.out.println("\n\nNew Game: [P] Purchase, [D] Draw, [W] Winners, [N] New Game, [V] View Tickets, [Q] Quit.");
-        ticketsSold = 0;
-        firstBall = 0;
-        secondBall = 0;
-        thirdBall = 0;
-        firstWinner = null;
-        secondWinner = null;
-        thirdWinner = null;
+        System.out.println("\n\nNew Lottery Started. Available commands:\n[P] Purchase, [D] Draw, [W] Winners, [N] New Game, [V] View Tickets, [Q] Quit.");
+        drawDone = false;
+        ticketsSold = firstBall = secondBall = thirdBall = 0;
+        firstWinner = secondWinner = thirdWinner = null;
         ticketList.clear();
         ticketsSoldList.clear();
         createTickets(ticketList, numTickets);
     }
 
     public static void findWinners(BigDecimal pot) {
-
         for (Ticket t : ticketsSoldList) {
             if (t.getTicketNumber() == firstBall) {
                 firstWinner = t;
                 firstWinner.setWinnings(pot.multiply(new BigDecimal("0.375")));
             }
-
             if (t.getTicketNumber() == secondBall) {
                 secondWinner = t;
                 secondWinner.setWinnings(pot.multiply(new BigDecimal("0.075")));
             }
-
             if (t.getTicketNumber() == thirdBall) {
                 thirdWinner = t;
                 thirdWinner.setWinnings(pot.multiply(new BigDecimal("0.05")));
@@ -130,24 +128,25 @@ public class Lottery {
             System.out.println(firstWinner.getName() + ": $" + firstWinner.getWinnings().toPlainString());
             pot = pot.subtract(firstWinner.getWinnings());
         }
-        else
+        else {
             System.out.println("No 1st winner.");
-
+        }
         if (secondWinner != null) {
             System.out.println(secondWinner.getName() + ": $" + secondWinner.getWinnings().toPlainString());
             pot = pot.subtract(secondWinner.getWinnings());
         }
-        else
+        else {
             System.out.println("No 2nd winner.");
-
+        }
         if (thirdWinner != null) {
             System.out.println(thirdWinner.getName() + ": $" + thirdWinner.getWinnings().toPlainString());
             pot = pot.subtract(thirdWinner.getWinnings());
         }
-        else
+        else {
             System.out.println("No 3rd winner.");
+        }
 
-        System.out.println("\nRemaining amount in pot for next game:" + pot.toPlainString());
+        System.out.println("\nRemaining amount in pot for next game: $" + pot.toPlainString());
         return pot;
     }
 }
